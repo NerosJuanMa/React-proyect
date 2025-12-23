ed594f9eb8e6af0b55b3edd5baee9b58695670d2
Neros__JuanMa <jmmudarra@gmail.com>
2025-12-23 14:25:37 +0100

    Update App.jsx

--- frontend/react_proyect/src/App.jsx
+++ frontend/react_proyect/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from "./components/header.jsx"
-import Home  from './pages/home.jsx'
+import Header from "./components/Header.jsx"
+import Home  from './pages/Home.jsx'
 
 function App() {
   return (
